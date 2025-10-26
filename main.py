f318dc7518850d45cd082c1bb3a6c360b548863c
sayem <sayem.eee04@gmail.com>
2025-10-26 13:20:00 -0400

    adding further capabilities

--- main.py
+++ main.py
@@ -20,11 +20,14 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock_object = pygame.time.Clock()
+    dt=0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock_object.tick(60)/1000
 if __name__ == "__main__":
     main()
